2b37256995e34940359a6c599c7956b7a9108d57
Samuel Mann <samuelcmann@gmail.com>
2024-05-22 17:19:40 -0400

    first commit

---
+++ examples/01_strip_on.py
@@ -0,0 +1,25 @@
+# import modules:
+import board
+import neopixel
+
+# create pixels object:
+pixels = neopixel.NeoPixel(
+    board.GP0,
+    9,
+    brightness=0.3,
+    auto_write=False
+)
+
+# set color for the entire strip (red):
+pixels.fill((255, 0, 0))
+
+# update the physical led strip:
+pixels.show()
+
+# try other colors:
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
