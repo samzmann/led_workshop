55679bd2ad77b60b7f63116efbaf8cacb7878037
Samuel Mann <samuelcmann@gmail.com>
2024-07-19 19:38:18 +0200

    better smoothing

--- raspberrypi_pico/examples/animation_breath.py
+++ raspberrypi_pico/examples/animation_breath.py
@@ -7,11 +7,12 @@ import math
 # define program constants
 NUM_PIXELS = 9
 COLOR = (255, 65, 0)
-BREATH_IN_DURATION_SECONDS = 1.0 * 2
-BREATH_OUT_DURATION_SECONDS = 3.0 * 2
+BREATH_IN_DURATION_SECONDS = 1.0
+BREATH_OUT_DURATION_SECONDS = 3.0
 GAMMA_CORRECTION = 2.8
-MIN_BRIGHTNESS = 0.2
+MIN_BRIGHTNESS = 0.05
 MAX_BRIGHTNESS = 1.0
+STEPS = 300
 
 # create pixels object:
 pixels = neopixel.NeoPixel(
@@ -27,24 +28,24 @@ pixels.show()
 
 def getGammaValue(i):
     """Calculate gamma corrected brightness."""
-    return math.pow(i / 100, GAMMA_CORRECTION)
+    return math.pow(i / STEPS, GAMMA_CORRECTION)
 
 def getGammaValueInverted(i):
     """Calculate inverted gamma corrected brightness."""
-    return 1 - math.pow((100 - i) / 100, GAMMA_CORRECTION)
+    return 1 - math.pow((STEPS - i) / STEPS, GAMMA_CORRECTION)
 
 # this loop runs forever, our program interaction lives in here:
 while True:
-    for i in range(0, 101):
+    for i in range(0, STEPS + 1):
         # calculate brightness with gamma correction
         brightness = MIN_BRIGHTNESS + (MAX_BRIGHTNESS - MIN_BRIGHTNESS) * getGammaValue(i)
         pixels.brightness = brightness
         pixels.show()
-        time.sleep(BREATH_IN_DURATION_SECONDS / 100)
+        time.sleep(BREATH_IN_DURATION_SECONDS / STEPS)
 
-    for i in range(100, -1, -1):
+    for i in range(STEPS, -1, -1):
         # calculate brightness with gamma correction
         brightness = MIN_BRIGHTNESS + (MAX_BRIGHTNESS - MIN_BRIGHTNESS) * getGammaValueInverted(i)
         pixels.brightness = brightness
         pixels.show()
-        time.sleep(BREATH_OUT_DURATION_SECONDS / 100)
+        time.sleep(BREATH_OUT_DURATION_SECONDS / STEPS)
